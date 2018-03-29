eb5421a06ee4a984e33946a57c13a4c4bbceffa6
aidensyn <37784266+aidensyn@users.noreply.github.com>
2018-03-28 21:06:17 -0700

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -35,7 +35,7 @@ create table Court
 
 create table CriminalRecordType 
 ( RecordID char(7) not null, 
-    TypeName char(10) not null, 
+    TypeName char(15) not null, 
     PRIMARY KEY (RecordID, TypeName), 
     foreign key (RecordID) references Record on delete cascade, 
     foreign key (TypeName) references CriminalType on delete cascade);
